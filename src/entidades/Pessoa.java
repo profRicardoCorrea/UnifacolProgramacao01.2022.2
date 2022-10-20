ebcd5d90f09a1a1f620770b8ba5497a7db7ddcb0
profRicardoCorrea <111370109+profRicardoCorrea@users.noreply.github.com>
2022-10-19 21:14:54 -0300

    jogador
    jogador

---
+++ src/entidades/Pessoa.java
@@ -0,0 +1,30 @@
+package entidades;
+
+public abstract class Pessoa {
+
+	private String nome;
+	private  String cpf;
+	public Pessoa(String nome, String cpf) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+	public Pessoa() {}
+	
+	public abstract String validarCpf(String cpf);
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+}
---
+++ src/enums/PosicaoJogador.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum PosicaoJogador {
+	MeioCampo,
+Atacante,
+Goleiro;
+}
