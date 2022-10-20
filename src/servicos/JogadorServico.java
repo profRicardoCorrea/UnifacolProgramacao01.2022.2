ff726aeab3eff003f7ed65b0b766252f54c77b99
profRicardoCorrea <111370109+profRicardoCorrea@users.noreply.github.com>
2022-10-19 23:49:52 -0300

    main
    main

--- src/servicos/JogadorServico.java
+++ src/servicos/JogadorServico.java
@@ -8,14 +8,14 @@ public class JogadorServico implements IJogadorServico {
 
 	@Override
 	public boolean contratar(Jogador jogador) {
-		System.out.println("Nome JOGADOR OBJETO:"+jogador.getNome());
+		System.out.println("Nome JOGADOR OBJETO: " + jogador.getNome());
 		return true;
 	}
 
 	@Override
 	public boolean contratar(String nome, String cpf, String time, PosicaoJogador posicao, int camisa) {
 		Jogador jog=new Jogador(nome, cpf, time, posicao, camisa);
-		System.out.println("Nome JOGADOR PARMS:"+jog.getNome());
+		System.out.println("Nome JOGADOR PARMS: " + jog.getNome());
 		return false;
 	}
 
